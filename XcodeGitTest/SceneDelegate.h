2f8bd2e9aff9b5c9399bbb351320ccbc28621667
zhangxin <zhangxin@tianan-cyber.com>
2021-02-03 11:50:20 +0800

    Initial Commit

---
+++ XcodeGitTest/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  XcodeGitTest
+//
+//  Created by zhangxin on 2021/2/3.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ XcodeGitTest/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  XcodeGitTest
+//
+//  Created by zhangxin on 2021/2/3.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
